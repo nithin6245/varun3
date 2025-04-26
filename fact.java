4a306059d26abbc47b7e1ed6f44b79b8e5e78097
nithin6245 <nithinkumar6245@gmail.com>
2025-04-26 08:03:25 +0530

    Add files via upload

---
+++ fact.java
@@ -0,0 +1,17 @@
+pakage =p1;
+import java.util.*;
+class fact1
+{
+public void nithin1()
+{
+Scanner sc=new Scanner(System.in);
+int n=sc.nextInt();
+if (n%2==0)
+{
+System.out.println("the number is even number");
+}
+else
+{
+System.out.println("the number is odd");
+} 
+}
